96749cc721a200dd39b7461a47dc508e119332d8
jiewei119 <810337170@qq.com>
2018-05-15 18:10:40 +0800

    首次提交

--- Example/Pods/Target Support Files/Pods-JWSQLiteSwift_Example/Pods-JWSQLiteSwift_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-JWSQLiteSwift_Example/Pods-JWSQLiteSwift_Example-frameworks.sh
@@ -102,9 +102,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/JWSQLiteSwift/JWSQLiteSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/JWSQLiteSwift/JWSQLiteSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
